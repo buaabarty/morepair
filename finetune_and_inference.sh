297916314eeca743fdca8d157d22e4305420e4d9
buaabarty <buaabarty@gmail.com>
2024-04-02 13:20:40 +0800

    update

--- finetune_and_inference.sh
+++ finetune_and_inference.sh
@@ -1,7 +1,7 @@
 rm tmp/* || mkdir tmp
 rm logs/* || mkdir logs
 mkdir models || true
-if [[ $3 == *"baseline" ]]; then
+if [[ $3 != *"baseline" ]]; then
     python3 MOTrain.py $1 data/trainset/$2.json models/$3 $4
 fi
 python3 inference_cpp.py $3
